133fb1087f46207a133d471d9239bccd993f6a98
强淑婷 <863139668@qq.com>
2018-08-06 19:19:09 +0800

    Initial Commit

---
+++ 考核/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  考核
+//
+//  Created by 强淑婷 on 2018/8/6.
+//  Copyright © 2018年 强淑婷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
